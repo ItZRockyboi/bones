c09e0b4126ab3b6ed34467362323d155a7b07180
ItZRockyboi <112095621+ItZRockyboi@users.noreply.github.com>
2023-03-13 17:29:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var song = "";
+function setup(){
+canvas = createCanvas(600,500);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+
+}
+
+
+function draw(){
+image(video,0,0,600,500);
+}
+
+
+function preload(){
+song = loadSound("Bones.mp3");
+}
+
+
+function play() {
+    song.play()
+}
+
+
+function stop() {
+    song.stop()
+}
